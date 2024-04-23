d11e5a0522733beab0b62d8b50c6d633a3a94e77
fabiano <jeovajire2003@gmail.com>
2024-04-22 21:54:32 -0300

    firt commit

---
+++ src/test/java/com/fabianospdev/volunteer/VolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabianospdev.volunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
