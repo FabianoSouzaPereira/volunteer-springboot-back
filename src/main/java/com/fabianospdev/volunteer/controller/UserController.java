129d2de52bef56ed39147483a5a23cd89a5acc4b
fabiano <jeovajire2003@gmail.com>
2024-04-24 15:00:28 -0300

    add UserDTO

--- src/main/java/com/fabianospdev/volunteer/controller/UserController.java
+++ src/main/java/com/fabianospdev/volunteer/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.fabianospdev.volunteer.controller;
 
 import com.fabianospdev.volunteer.domain.models.User;
+import com.fabianospdev.volunteer.dto.UserDTO;
 import com.fabianospdev.volunteer.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -19,8 +21,9 @@ public class UserController {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body( list );
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect( Collectors.toList());
+        return ResponseEntity.ok().body( listDto );
     }
 }
