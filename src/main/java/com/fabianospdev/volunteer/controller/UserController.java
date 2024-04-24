b705cc0a5c2768b2cebe75195f64f59c96771816
fabiano <jeovajire2003@gmail.com>
2024-04-24 17:35:40 -0300

    delete user service

--- src/main/java/com/fabianospdev/volunteer/controller/UserController.java
+++ src/main/java/com/fabianospdev/volunteer/controller/UserController.java
@@ -41,4 +41,10 @@ public class UserController {
         return ResponseEntity.created(uri).build();
     }
 
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/fabianospdev/volunteer/services/UserService.java
+++ src/main/java/com/fabianospdev/volunteer/services/UserService.java
@@ -5,7 +5,11 @@ import com.fabianospdev.volunteer.dto.UserDTO;
 import com.fabianospdev.volunteer.repositories.UserRepository;
 import com.fabianospdev.volunteer.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +31,11 @@ public class UserService {
 
     public User insert(User obj){
         return repository.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repository.deleteById(id);
     }
 
     public User fromDTO( UserDTO objDto) {
