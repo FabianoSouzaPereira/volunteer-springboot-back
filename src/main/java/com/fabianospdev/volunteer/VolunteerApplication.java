92d2aca2799b2fdaa17d639f985e092a42c2026f
fabiano <jeovajire2003@gmail.com>
2024-05-01 10:18:14 -0300

    removed EnableMongoRepositories, project doesn't run with it.

--- src/main/java/com/fabianospdev/volunteer/VolunteerApplication.java
+++ src/main/java/com/fabianospdev/volunteer/VolunteerApplication.java
@@ -1,10 +1,7 @@
 package com.fabianospdev.volunteer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class VolunteerApplication {
