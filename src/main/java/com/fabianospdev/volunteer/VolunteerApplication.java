5520220197a9332333ad0ebce611ce62d78a961c
Fabiano de Souza Pereira <jeovajire2003@gmail.com>
2026-09-06 17:20:20 -0300

    Removed @EnableAutoConfiguration extra and pass exclude to @SpringBootApplication.

--- src/main/java/com/fabianospdev/volunteer/VolunteerApplication.java
+++ src/main/java/com/fabianospdev/volunteer/VolunteerApplication.java
@@ -1,12 +1,10 @@
 package com.fabianospdev.volunteer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class VolunteerApplication{
 
     public static void main(String[] args) {
--- src/test/java/com/fabianospdev/volunteer/VolunteerApplicationTests.java
+++ src/test/java/com/fabianospdev/volunteer/VolunteerApplicationTests.java
@@ -1,13 +1,11 @@
 package com.fabianospdev.volunteer;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 class VolunteerApplicationTests{
 
     @Test
